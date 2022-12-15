39c25ab6eb2a18fbd646cdfb2a38744841ad8cc1
준상 유 <ybrother8143@gmail.com>
2022-12-15 14:32:42 +0900

    interval 선택에 따른 값 뽑아내기

--- excel/gocator.py
+++ excel/gocator.py
@@ -1,16 +1,6 @@
 # -*- coding: utf-8 -*-
-import pandas as pd
-import os
-import glob
-import natsort
-import sys
-import numpy as np
 
-sys.path.append(os.path.dirname(os.path.dirname(__file__)))
-from file import rawfile
-
-if __name__ == "__main__":
-    """IMU 파일의 interval 을 기준으로 csv 중간 column의 데이터 값과 Z_Acc 값 정리 
+"""IMU 파일의 interval 을 기준으로 csv 중간 column의 데이터 값과 Z_Acc 값 정리
         저장파일 포멧 : .raw
         [Laser,Acceleration,TriggerInfo,FlagIndex,Section]
         Laser: csv 파일은 5mm 간격
@@ -18,11 +8,28 @@ if __name__ == "__main__":
         TriggerInfo: 0
         FlagIndex: 0
         Section: 0
+"""
+
+import pandas as pd
+import os
+import glob
+import natsort
+import sys
+import numpy as np
+
+from file import rawfile
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+
+
+def acc_interval(anal_path: str, acc_intervals: list, use_indiviual_interval: bool = False):
+    """Acceleration_Z(g)의 interval을 기준으로 laser 데이터 값 정리하여 raw 파일로 저장하는 함수
+    Args:
+        anal_path: 조사경로(e.g: D:/dataset/조사1), .krs 파일 경로의 상위 경로
+        acc_intervals: raw 파일에 저장할 acc_interval 간격, 지정안하면, 50mm 기준
+
+    :return: None
     """
 
-    # 10km/h, 20km/h, 30km/h, 40km/h, 50km/h (mm)
-    intervals = [2.7, 5.5, 8.3, 11.1, 13.8]
-    anal_path = r'D:\data\20221206_gocator교정데이터'
     result_path = os.path.join(anal_path, 'rawfile_results')
     os.makedirs(result_path, exist_ok=True)
 
@@ -34,62 +41,39 @@ if __name__ == "__main__":
     if not gocator_paths:
         sys.exit('Gocator_v7 폴더가 존재하지 않습니다.')
 
-    for interval, gocator_path, imu_path in zip(intervals, gocator_paths, imu_paths):
+    for gocator_index, gocator_path in enumerate(gocator_paths):
+        # Rawfile path 지정
+        anal_name = os.path.basename(os.path.dirname(gocator_path))
+        sensor_location = os.path.basename(gocator_path)
+        rawfile_path = os.path.join(result_path, (anal_name + '_' + sensor_location + '.raw'))
+        print(fr"{anal_name + '_' + sensor_location} 진행중.... {gocator_index + 1}/{len(gocator_paths)}")
+
         csv_files = glob.glob(gocator_path + r'\*Profile\*.csv')
-        imu_file = glob.glob(imu_path + fr'\*{os.path.basename(gocator_path)}.txt')
-        csv_files = natsort.natsorted(csv_files)[:10]
+        imu_file = glob.glob(imu_paths[gocator_index] + fr'\*{os.path.basename(gocator_path)}.txt')
+        if not csv_files:
+            sys.exit('csv 파일이 존재하지 않습니다.')
+        if not imu_file:
+            sys.exit('IMU 파일이 존재하지 않습니다.')
+
+        csv_files = natsort.natsorted(csv_files)
 
         # Read imu file's "Accelero_Z(g)" column
         imu_df = pd.read_csv(imu_file[0], usecols=[6])
         csv_df = pd.DataFrame()
         for csv_file in csv_files:
-            csv_df = csv_df.append(pd.read_csv(csv_files[0], header=None, usecols=[2999]))
+            csv_df = csv_df.append(pd.read_csv(csv_file, header=None, usecols=[2999]))
 
-        # Rawfile path 지정
-        anal_name = os.path.basename(os.path.dirname(gocator_path))
-        sensor_location = os.path.basename(gocator_path)
-        rawfile_path = os.path.join(result_path, (anal_name + '_' + sensor_location + '.raw'))
-        rawfile.RawFile(rawfile_path)
+        rawfile_class = rawfile.RawFile(
+            file_path=rawfile_path, csv_df=csv_df, imu_df=imu_df, interval=acc_intervals[gocator_index])
+        rawfile_class.write_rawfile(use_indiviual_interval)
 
-        raw_file = open(rawfile_path, "a")
-        csv_index = 0
-        imu_index = 0
-        TriggerInfo_index = 1
 
-        while True:  # [mm], 100m 초과 시 종료
-            if imu_index == 0:
-                raw_file.write(f'{csv_df.values[csv_index][0]},{imu_df.values[imu_index][0]},0,0,0\n')
-                csv_index += 1
-                imu_index += 1
-                continue
+if __name__ == "__main__":
+    # 10km/h, 20km/h, 30km/h, 40km/h, 50km/h (mm)
+    intervals = [2.778, 5.5, 8.3, 11.1, 13.8]
+    input_path = r'D:\data\20221206_gocator교정데이터'
 
-            imu_distance = imu_index * interval  # [mm]
-            csv_distance = csv_index * 5  # [mm]
-            diff = csv_distance - imu_distance
-
-            if imu_distance > 100000:
-                break
-
-            if TriggerInfo_index == 9:
-                TriggerInfo_index = 0
-
-            if diff < -3.5:
-                csv_index += 1
-                continue
-            elif -3.5 <= diff < -1.5:
-                raw_file.write(f'{round(np.mean(csv_df.values[csv_index-1:csv_index+1]), 4)},{imu_df.values[imu_index][0]},{TriggerInfo_index},0,0\n')
-            elif -1.5 <= diff < 0:
-                raw_file.write(f'{csv_df.values[csv_index][0]},{imu_df.values[imu_index][0]},{TriggerInfo_index},0,0\n')
-            elif 0 <= diff <= 1.5 and csv_index < 20000:
-                raw_file.write(f'{csv_df.values[csv_index][0]},{imu_df.values[imu_index][0]},{TriggerInfo_index},0,0\n')
-            elif 1.5 < diff <= 3.5:
-                raw_file.write(f'{round(np.mean(csv_df.values[csv_index:csv_index+2]), 4)},{imu_df.values[imu_index][0]},{TriggerInfo_index},0,0\n')
-            elif 3.5 < diff:
-                csv_index -= 1
-                continue
-
-            imu_index += 1
-            csv_index += 1
-            TriggerInfo_index += 1
-
-        raw_file.close()
+    # km/h 별 interval 적용
+    acc_interval(anal_path=input_path, acc_intervals=intervals, use_individual_interval=False)
+    # interval 50mm 고정
+    # acc_interval(anal_path=input_path, acc_intervals=intervals, use_indiviual_interval=False)
--- file/rawfile.py
+++ file/rawfile.py
@@ -1,15 +1,136 @@
-class RawFile:
-    """Read & Write rawfile
-    Args:
-        filename: Saving raw filename of file (e.g: test.raw)
+import numpy as np
 
-    """
-    def __init__(self, filename: str):
+
+class RawFile:
+    def __init__(
+            self, file_path: str, csv_df: np.array, imu_df: np.array, interval: int):
+        """Read & Write rawfile
+
+        :param file_path: raw format file path
+        :param csv_df: DataFrame, csv file(only laser data, [5mm])
+        :param imu_df: DataFrame, imu file(only Acc_Z, imu data, [individual interval])
+        :param interval: Imu data's interval
+        """
+        # init parameters
+        self.file_path = file_path
+        self.csv_df = csv_df
+        self.imu_df = imu_df
+        self.interval = interval
+
+        self.csv_index = 0
+        self.imu_index = 0
+        self.trigger_info_index = 1
+
+        self.max_csv_df = len(csv_df) * 5
+        self.max_imu_df = len(imu_df) * self.interval
+
         # Writing raw file
-        with open(filename, "w") as raw_file:
-            raw_file.write('RTSensorRaw\n')
-            raw_file.write('File Version,4\n')
-            raw_file.write('Interval[mm],5\n')
-            raw_file.write('ValueCount,1\n')
-            raw_file.write('VarType,US,US,SI,SI,US\n')
-            raw_file.write('Laser,Acceleration,TriggerInfo,FlagIndex,Section\n')
+        with open(self.file_path, "w") as file:
+            file.write('RTSensorRaw\n')
+            file.write('File Version,4\n')
+            file.write('Interval[mm],50\n')
+            file.write('ValueCount,1\n')
+            file.write('VarType,US,US,SI,SI,US\n')
+            file.write('Laser,Acceleration,TriggerInfo,FlagIndex,Section\n')
+
+    def find_diff(self):
+        imu_distance = self.imu_index * self.interval  # [mm]
+        csv_distance = self.csv_index * 5  # [mm]
+        return csv_distance - imu_distance
+
+    def individual_interval(self, raw_file):
+        """Writing rawfile contents in while loop with individual intervals
+
+        :param raw_file: File for writing contents(opened)
+        :return: bool, False: continue external while loop
+        """
+        diff = self.find_diff()
+        if diff < -3.5:
+            self.csv_index += 1
+            return False
+        elif -3.5 <= diff < -1.5:
+            raw_file.write(
+                f'{round(np.mean(self.csv_df.values[self.csv_index - 1:self.csv_index + 1]), 4)},'
+                f'{self.imu_df.values[self.imu_index][0]},{self.trigger_info_index},0,0\n')
+        elif -1.5 <= diff < 0:
+            raw_file.write(
+                f'{self.csv_df.values[self.csv_index][0]},'
+                f'{self.imu_df.values[self.imu_index][0]},{self.trigger_info_index},0,0\n')
+        elif 0 <= diff <= 1.5 and self.csv_index < 20000:
+            raw_file.write(
+                f'{self.csv_df.values[self.csv_index][0]},'
+                f'{self.imu_df.values[self.imu_index][0]},{self.trigger_info_index},0,0\n')
+        elif 1.5 < diff <= 3.5:
+            raw_file.write(
+                f'{round(np.mean(self.csv_df.values[self.csv_index:self.csv_index + 2]), 4)},'
+                f'{self.imu_df.values[self.imu_index][0]},{self.trigger_info_index},0,0\n')
+        elif 3.5 < diff:
+            self.csv_index -= 1
+            return False
+        self.csv_index += 1
+        return True
+
+    def fixed_interval(self, raw_file):
+        """Writing rawfile contents in while loop with fixed interval[50mm]
+        50mm의 laser data interval 마다 값 저장
+
+        :param raw_file: File for writing contents(opened)
+        :return: bool, False: continue external while loop
+        """
+        while abs(self.find_diff()) > self.interval:
+            self.imu_index += 1
+
+        if (self.csv_index * 5 >= self.max_csv_df) or ((self.imu_index * self.interval) >= self.max_imu_df):
+            return False
+
+        ex_diff = abs(self.find_diff() - 1)
+        if self.imu_index != len(self.imu_df.values):
+            self.imu_index += 1
+        current_diff = abs(self.find_diff() - 1)
+
+        if ex_diff <= current_diff:
+            imu_index = self.imu_index - 1
+        else:
+            imu_index = self.imu_index
+
+        raw_file.write(
+            f'{self.csv_df.values[self.csv_index][0]},'
+            f'{self.imu_df.values[imu_index][0]},{self.trigger_info_index},0,0\n')
+        # print(fr"index:{imu_index}, distance:{imu_index * self.interval}")
+        self.csv_index += 10
+        return True
+
+    def write_rawfile(self, use_individual_interval: bool):
+        """Writing rawfile mother function
+
+        :param use_individual_interval: bool
+        :return:
+        """
+        raw_file = open(self.file_path, "a")
+        while True:  # [mm]
+            if (self.csv_index * 5 >= self.max_csv_df) or ((self.imu_index * self.interval) >= self.max_imu_df):
+                break
+
+            if self.imu_index == 0:
+                raw_file.write(
+                    f'{self.csv_df.values[self.csv_index][0]},{self.imu_df.values[self.imu_index][0]},0,0,0\n')
+                self.csv_index += 1
+                self.imu_index += 1
+                continue
+
+            if self.trigger_info_index == 10:
+                self.trigger_info_index = 0
+
+            #  Writing raw file
+            if use_individual_interval:  # Acc_Z interval 기준
+                if not self.individual(raw_file):
+                    continue
+            else:  # 50mm interval 기준
+                if self.csv_index == 1:
+                    self.csv_index = 10
+                if not self.fixed_interval(raw_file):
+                    break
+
+            self.imu_index += 1
+            self.trigger_info_index += 1
+        raw_file.close()
